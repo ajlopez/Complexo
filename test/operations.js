86e95f74bee4beab2a382a3b2bb68a6c17afb4b8
ajlopez <webmaster@ajlopez.com>
2014-04-01 07:20:13 -0300

    Refactor operation names; multiply first test

--- test/operations.js
+++ test/operations.js
@@ -6,7 +6,7 @@ exports['create complex number'] = function (test) {
     
     test.ok(cnumber);
     test.equal(cnumber.real(), 1);
-    test.equal(cnumber.imaginary(), 2);
+    test.equal(cnumber.imag(), 2);
 };
 
 exports['add two complex numbers'] = function (test) {
@@ -17,16 +17,27 @@ exports['add two complex numbers'] = function (test) {
     
     test.ok(result);
     test.equal(result.real(), 4);
-    test.equal(result.imaginary(), 6);
+    test.equal(result.imag(), 6);
 };
 
 exports['subtract two complex numbers'] = function (test) {
     var cnumber1 = cx.create(1, 2);
     var cnumber2 = cx.create(3, 7);
     
-    var result = cnumber1.subtract(cnumber2);
+    var result = cnumber1.sub(cnumber2);
     
     test.ok(result);
     test.equal(result.real(), -2);
-    test.equal(result.imaginary(), -5);
+    test.equal(result.imag(), -5);
+};
+
+exports['multiply two complex numbers'] = function (test) {
+    var cnumber1 = cx.create(2, 3);
+    var cnumber2 = cx.create(3, 7);
+    
+    var result = cnumber1.mult(cnumber2);
+    
+    test.ok(result);
+    test.equal(result.real(), 2*3 - 3*7);
+    test.equal(result.imag(), 3*3 + 2*7);
 };
