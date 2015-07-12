4375c7db26052b0a5b1a3e9149a377c7f7cdee08
ajlopez <webmaster@ajlopez.com>
2015-07-12 08:43:08 -0300

    Constants: zero, one, i

---
+++ test/constants.js
@@ -0,0 +1,23 @@
+
+var cx = require('..');
+
+exports['zero'] = function (test) {
+    var zero = cx.ZERO;
+    
+    test.equal(zero.real(), 0);
+    test.equal(zero.imag(), 0);
+};
+
+exports['one'] = function (test) {
+    var zero = cx.ONE;
+    
+    test.equal(zero.real(), 1);
+    test.equal(zero.imag(), 0);
+};
+
+exports['i'] = function (test) {
+    var i = cx.I;
+    
+    test.equal(i.real(), 0);
+    test.equal(i.imag(), 1);
+};
